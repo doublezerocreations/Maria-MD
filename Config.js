afe3f44542a5ba7a46f01e3f657868fe07da9fe8
doublezerocreations <91972089+doublezerocreations@users.noreply.github.com>
2024-04-06 20:19:28 +0530

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,21 +2,21 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
-global.ytname = "YT: AyushBotz.inc"
-global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.ownernumber = ['94765714446']
+global.ownername = "💙𓆩‎✘𝕯𝖆𝖘𝖚𝖓 𝕯𝖚𝖑𝖆𝖗𝖆𝖐𝖆✘𓆪_✰"//owner name
+global.ytname = "YT: Double Zero Creations"
+global.socialm = "Facebook: Dasun Dularaka"
+global.location = "Sri Lankan"
 
-global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
+global.botname = 'ᴅᴏᴜʙʟᴇ-ᴢᴇʀᴏᴹᴰ' //name of the bot
 
 //sticker details
-global.stickername = '💙𓆩‎✘AʏᴜSʜ✘𓆪_✰'
+global.stickername = '💙𓆩‎✘𝕯𝖆𝖘𝖚𝖓 𝕯𝖚𝖑𝖆𝖗𝖆𝖐𝖆✘𓆪_✰'
 global.packname = 'Sticker By'
-global.author = 'Maria Bot'
+global.author = 'Double Zero Bot'
 //console view/theme
 global.themeemoji = '🧩'
-global.wm = "Ayush botz inc."
+global.wm = "Double Zero Creations"
 
 //theme link
 global.link = 'https://whatsapp.com/channel/0029VaImo5ZG3R3qjKOdyr1I'
@@ -30,7 +30,7 @@ global.autoRecording = false //auto recording
 global.autoTyping = false //auto typing
 global.autorecordtype = false //auto typing + recording
 global.autoread = false //auto read messages
-global.autobio = false //auto update bio
+global.autobio = true //auto update bio
 global.anti212 = true //auto block +212
 global.autoread_status = false //auto view status/story
 
@@ -38,18 +38,18 @@ global.autoread_status = false //auto view status/story
 
 //reply messages
 global.mess = {
-    done: '*here you go!* \n\n*🎀 Maria Bot 🎀*\n\n*🧩 Bot link:* \nhttps://github.com/AYUSH-PANDEY023/Maria-MD\n',
+    done: '*here you go!* \n\n*🎀 Double Zero Bot 🎀*\n\n*🧩 Facebook:* \nhttps://facebook.com/double.zero.creations/\n',
     prem: '*This feature can be used by premium user only*',
     admin: '*This feature can be used by admin only*',
     botAdmin: '*This feature can only be used when the bot is a group admin* ',
-    owner: '*This feature can be used by owner only*',
+    owner: '*This feature can be used by my owner only*',
     group: '*This feature is only for groups*',
     private: '*This feature is only for private chats*',
     wait: '*In process...* ',    
     error: '*Error!*',
 }
 
-global.thumb = fs.readFileSync('./Gallery/thumb.jpg')
+global.thumb = fs.readFileSync('https://scontent.fcmb4-2.fna.fbcdn.net/v/t39.30808-6/423714807_721700460102970_5690121493739821910_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFmR9GM29XEmsNSUgLsFoUQGO-tVC54haIY761ULniFotPa22__4xrOVxqUKbThUJgdp6TI9pIEul14d710QMjx&_nc_ohc=jw4uC9ErVyIAX9tKX7V&_nc_zt=23&_nc_ht=scontent.fcmb4-2.fna&oh=00_AfAi7FmhP8jMPFdqQd_yI16KGp5QOEyZcTVq-7hUUJV31g&oe=65FFBEE5')
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
